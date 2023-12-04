ae0b1a81f8ae7d77c6330a2e4d7a48d28fa6b399
Mckupe <nohrin09072001@yandex.ru>
2023-12-04 22:51:22 +0500

    version 0.2

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM python:3.11
+
+# ARG YOUR_ENV
+
+# RUN apt update
+# RUN apt upgrade -y
+# RUN curl -sSL https://install.python-poetry.org | python3 -
+
+# WORKDIR /code
+# COPY poetry.lock pyproject.toml /code/
+
+# COPY . /code
+
+# ENTRYPOINT ["poetry", "run", "python", "-m", "annapurna.main"]
+
+# CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
+
+WORKDIR /code
+
+COPY poetry.lock pyproject.toml ./
+
+RUN apt update
+RUN apt upgrade -y
+RUN python -m pip install --no-cache-dir poetry==1.4.2 \
+    && poetry config virtualenvs.create false \
+    && poetry install --without dev,test --no-interaction --no-ansi \
+    && rm -rf $(poetry config cache-dir)/{cache,artifacts}
+
+COPY __init__.py ./
+
+CMD ["uvicorn", "__init__:app", "--host", "0.0.0.0", "--port", "80"]
+
+# USER get-images
---
+++ __init__.py
@@ -0,0 +1,82 @@
+import datetime
+import io
+import json
+from multiprocessing import Pool
+import multiprocessing as mp
+import zipfile
+from fastapi import FastAPI, Response
+from fastapi.responses import StreamingResponse
+import requests
+from functools import partial
+
+app = FastAPI()
+file = open('lines.json')
+lines = json.load(file)
+
+def get_Packs(ip):
+    try:
+        response = requests.get(f'http://{ip}/api/v1_0/packs_queue')
+    except requests.HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+        return http_err
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+        return err
+    else:
+        responseJS = json.loads(response.content)
+        return(len(responseJS))
+
+def get_Mulps(ip):
+    try:
+        response = requests.get(f'http://{ip}/api/v1_0/multipacks_queue')
+    except requests.HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+        return http_err
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+        return err
+    else:
+        responseJS = json.loads(response.content)
+        return(len(responseJS))
+
+def f(x,url):
+    id = x['Monitor']['Id']
+    print(id)
+    response = requests.get(url + str(id))
+    img = io.BytesIO(response.content)
+    return((id,img))
+
+@app.get("/get_images/{ip}",response_class=Response)
+async def read_root(ip: str):
+    if ip not in lines:
+        return "Не найдена данная линия"
+    backIp = lines[ip]["back"]
+    zmIp = lines[ip]["zm"]
+    url = f'http://{zmIp}/zm/cgi-bin/nph-zms?mode=single&monitor='
+    zip_buffer = io.BytesIO()
+    try:
+        response = requests.get(f'http://{zmIp}/zm/api/monitors.json')
+    except requests.HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+        return http_err
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+        return err
+    else:
+        print('Success!')
+        zip_buffer = io.BytesIO()
+        zip_file = zipfile.ZipFile(zip_buffer, "a", zipfile.ZIP_DEFLATED, True)
+        func = partial(f, url = url)
+        if __name__ == '__init__':
+            with Pool(mp.cpu_count()) as p:
+                fotos = p.map(func, json.loads(response.content)['monitors'])
+        # print(fotos)
+        for file_name, file_object in fotos:
+            zip_file.writestr(f"{file_name}.jpeg", file_object.getvalue())
+        zip_file.writestr(f"status.txt", str(get_Packs(backIp)) + " / " + str(get_Mulps(backIp)))
+        zip_file.close()
+        return StreamingResponse(
+            iter([zip_buffer.getvalue()]), 
+            media_type="application/x-zip-compressed", 
+            headers = { "Content-Disposition": f"attachment; filename={datetime.datetime.now()}.zip"}
+        )
